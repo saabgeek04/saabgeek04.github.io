18a6d3fbe17f954b1043b3f80e9f788310fed632
saabgeek04 <115202864+saabgeek04@users.noreply.github.com>
2022-10-11 09:18:11 +1100

    Update draggy.js

--- draggy.js
+++ draggy.js
@@ -1,6 +1,7 @@
 // Make the DIV element draggable:
 dragElement(document.getElementById("mydiv"));
 
+
 function dragElement(elmnt) {
   var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
   if (document.getElementById(elmnt.id + "header")) {
