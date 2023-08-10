e62f48dbefe43d09e1864cb111601a4bd56f2aa2
saabgeek04 <115202864+saabgeek04@users.noreply.github.com>
2023-08-10 12:08:12 +1000

    card rotation testing

---
+++ sillycard.js
@@ -0,0 +1,44 @@
+console.log('JS started');
+
+const blah = document.querySelector('#sillycard');
+
+blah.innerHTML += '<p> Testing Text Content </p>';
+
+const penda = document.createElement('article');
+
+penda.innerHTML = ''
+penda.style.opacity = '1'
+blah.appendChild(penda)
+
+/**
+ * @param {MouseEvent} event 
+ */
+
+penda.onmousemove = rotator;
+penda.onmouseleave = unrotator;
+
+/**
+ * @param {PointerEvent} event 
+ */
+function rotator(event) {
+
+    const posbox = penda.getBoundingClientRect();
+
+    const relcursorX = event.clientX - posbox.x;
+    const relcursorY = event.clientY - posbox.y;
+
+    const normcurseX = relcursorX / posbox.width;
+    const normcurseY = relcursorY / posbox.height;
+
+    const Xfactor = ( normcurseX * 2 - 1 ) * 30;
+    const Yfactor = ( normcurseY * 2 - 1 ) * -30;
+    
+    penda.style.transform = 'rotateY(' + Xfactor.toString() + 'deg) rotateX(' + Yfactor.toString() + 'deg)';
+
+
+}
+
+function unrotator(event) {
+    penda.style.transform = null
+    penda.style.transition = '70ms'
+}
